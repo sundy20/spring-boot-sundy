f5d36177c5e98d085cc623b1b3ad21f2cac1b3b8
plus <zeng.wang@ele.me>
2019-03-21 11:00:22 +0800

    add

--- springboot-dubbo-provider/src/main/java/com/sundy/boot/utils/ThreadPollUtil.java
+++ springboot-dubbo-provider/src/main/java/com/sundy/boot/utils/ThreadPollUtil.java
@@ -11,7 +11,16 @@ public class ThreadPollUtil {
 
     public static ThreadPoolExecutor defineThreadPool(int coreSize, String namePrefix) {
 
-        return new ThreadPoolExecutor(coreSize, coreSize, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>(1024), new NamedThreadFactory(namePrefix), new ThreadPoolExecutor.AbortPolicy());
+        return new ThreadPoolExecutor(coreSize, coreSize, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>(1024),
+                new NamedThreadFactory(namePrefix), new ThreadPoolExecutor.AbortPolicy());
+    }
+
+    public static ThreadPoolExecutor defineThreadPool(int coreSize, String namePrefix, long keepAliveTime,
+                                                      TimeUnit timeUnit, int queueSize,
+                                                      ThreadPoolExecutor.AbortPolicy abortPolicy) {
+
+        return new ThreadPoolExecutor(coreSize, coreSize, keepAliveTime, timeUnit, new LinkedBlockingQueue<>(queueSize),
+                new NamedThreadFactory(namePrefix), abortPolicy);
     }
 
     public static ScheduledThreadPoolExecutor defineScheduledThreadPool(int coreSize, String namePrefix) {
