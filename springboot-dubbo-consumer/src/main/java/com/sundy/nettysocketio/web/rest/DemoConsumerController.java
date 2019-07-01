c5a4c45011eb23d0d51a5293f65d6bcef31fb732
plus <zeng.wang@ele.me>
2019-07-01 16:02:57 +0800

    hystrix 熔断测试

--- springboot-dubbo-consumer/src/main/java/com/sundy/nettysocketio/web/rest/DemoConsumerController.java
+++ springboot-dubbo-consumer/src/main/java/com/sundy/nettysocketio/web/rest/DemoConsumerController.java
@@ -1,7 +1,6 @@
 package com.sundy.nettysocketio.web.rest;
 
 import com.alibaba.dubbo.config.annotation.Reference;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.sundy.share.service.DemoService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,15 +17,8 @@ public class DemoConsumerController {
     @Reference(version = "1.0", check = false, timeout = 3000)
     private DemoService demoService;
 
-    @HystrixCommand(fallbackMethod = "reliable")
     @RequestMapping("/sayHello")
     public String sayHello(@RequestParam String name) {
-
         return demoService.sayHello(name);
-    }
-
-    public String reliable(String name) {
-
-        return "hystrix fallback value : " + name;
     }
 }
--- springboot-dubbo-provider/src/main/java/com/sundy/boot/dubbo/provider/ProductInfoServiceImpl.java
+++ springboot-dubbo-provider/src/main/java/com/sundy/boot/dubbo/provider/ProductInfoServiceImpl.java
@@ -1,6 +1,8 @@
 package com.sundy.boot.dubbo.provider;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.sundy.share.dto.ProductInfoDTO;
 import com.sundy.share.service.ProductInfoService;
 
@@ -16,6 +18,26 @@ public class ProductInfoServiceImpl implements ProductInfoService {
      *
      * @param productId 商品id
      */
+    @HystrixCommand(fallbackMethod = "fallback", groupKey = "ProductInfoService", commandKey = "GetProductInfoCommand",
+            commandProperties = {
+                    //滑动窗口中，最少有20个请求，才可能触发断路
+                    @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "20"),
+                    //异常比例达到多少，才触发断路，默认50%
+                    @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "40"),
+                    //断路后多少时间内直接reject请求，之后进入half-open状态，默认5000ms
+                    @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "3000"),
+                    //设置超时时间800ms（超过800ms走fallback） 默认1秒
+                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "800"),
+                    //设置fallback最大请求并发数
+                    @HystrixProperty(name = "fallback.isolation.semaphore.maxConcurrentRequests", value = "50")
+
+            },
+            threadPoolProperties = {
+                    @HystrixProperty(name = "coreSize", value = "16"),
+                    @HystrixProperty(name = "maximumSize", value = "16"),
+                    @HystrixProperty(name = "maxQueueSize", value = "512"),
+                    @HystrixProperty(name = "queueSizeRejectionThreshold", value = "512"),
+            })
     @Override
     public ProductInfoDTO getProductInfo(Long productId) {
         ProductInfoDTO productInfoDTO = new ProductInfoDTO();
