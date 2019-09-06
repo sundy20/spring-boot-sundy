88be7749b4db47adb4f94b5e77c70bfd40f3f134
plus <zeng.wang@ele.me>
2019-09-06 18:10:19 +0800

    guava retry

---
+++ springboot-dubbo-consumer/src/test/java/com/sundy/nettysocketio/RetryingTest.java
@@ -0,0 +1,59 @@
+package com.sundy.nettysocketio;
+
+import com.github.rholder.retry.*;
+import com.google.common.collect.Lists;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author zeng.wang
+ * @description guava-retrying测试
+ * @date 2019-09-06
+ */
+public class RetryingTest {
+
+    public static void main(String[] args) {
+        testRetrying();
+    }
+
+    public static void testRetrying() {
+        Retryer<List<String>> retryer = RetryerBuilder.<List<String>>newBuilder()
+                //当返回的结果列表为空时，需要重试
+                .retryIfResult(resultList -> resultList == null || resultList.size() == 0)
+                //当调用方法遇到RuntimeException异常时，重试
+                .retryIfExceptionOfType(RuntimeException.class)
+                //当重试次数达到3次时，停止重试
+                .withStopStrategy(StopStrategies.stopAfterAttempt(20))
+                //每次重试间隔的时长为递增       fixedWait(1000, TimeUnit.MILLISECONDS)为1000毫秒
+//                .withWaitStrategy(WaitStrategies.incrementingWait(1000, TimeUnit.MILLISECONDS, 2000, TimeUnit
+//                .MILLISECONDS))
+                //等待斐波那契数列时长
+                .withWaitStrategy(WaitStrategies.fibonacciWait(1000, 3, TimeUnit.MINUTES))
+                //每次重试间隔需要暂停当前线程
+                .withBlockStrategy(BlockStrategies.threadSleepStrategy())
+                //每次重试时，需要调用的重试监听器
+                .withRetryListener(new RetryListener() {
+                    @Override
+                    public <V> void onRetry(Attempt<V> attempt) {
+                        System.out.println("attemptNumber:" + attempt.getAttemptNumber() + ",delaySinceFirstAttempt:" + attempt.getDelaySinceFirstAttempt());
+                    }
+                })
+                .build();
+        try {
+            retryer.call(RetryingTest::buildNames);
+        } catch (ExecutionException | RetryException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static List<String> buildNames() {
+        System.out.println(Thread.currentThread().getName() + " exeucte ...");
+        //故意抛出异常
+        int i = 1 / 0;
+        return Lists.newArrayList("sundy", "jack");
+    }
+
+
+}
--- springboot-dubbo-provider/src/main/java/com/sundy/boot/domain/UserService.java
+++ springboot-dubbo-provider/src/main/java/com/sundy/boot/domain/UserService.java
@@ -1,33 +1,50 @@
 package com.sundy.boot.domain;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.RandomUtils;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Nonnull;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @Component
 public class UserService {
+    private long timeOut = RandomUtils.nextLong(1000, 10000);
+    private long defaultTime = 5 * 60 * 1000;
+    private LoadingCache<String, UserBean> guavaCache = CacheBuilder.newBuilder()
+            .refreshAfterWrite(defaultTime + timeOut, TimeUnit.MILLISECONDS)
+            .maximumSize(1000)
+            .build(new CacheLoader<String, UserBean>() {
+                @Override
+                public UserBean load(@Nonnull String key) {
+                    return getUser(key);
+                }
+            });
+
+    private Cache<String, UserBean> caffeineCache = Caffeine.newBuilder()
+            .refreshAfterWrite(defaultTime + timeOut, TimeUnit.MILLISECONDS)
+            .maximumSize(1000)
+            .build(this::getUser);
 
     public UserBean getUser(String username) {
-
         // 没有此用户直接返回null
-        if (!DataSource.getData().containsKey(username))
-
+        if (!DataSource.getData().containsKey(username)) {
             return null;
-
+        }
         UserBean user = new UserBean();
-
         Map<String, String> detail = DataSource.getData().get(username);
-
         user.setUsername(username);
-
         user.setPassword(detail.get("password"));
-
         user.setRole(detail.get("role"));
-
         user.setPermission(detail.get("permission"));
-
         user.setUserId(Long.valueOf(detail.get("id")));
-
         return user;
     }
 }
