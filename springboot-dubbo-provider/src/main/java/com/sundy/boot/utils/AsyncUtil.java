086164e258f40f53f0d82527de9f9175c7a5a017
zeng.wang <wz193281@alibaba-inc.com>
2020-04-23 11:31:50 +0800

    validate

--- springboot-dubbo-provider/src/main/java/com/sundy/boot/utils/AsyncUtil.java
+++ springboot-dubbo-provider/src/main/java/com/sundy/boot/utils/AsyncUtil.java
@@ -25,30 +25,49 @@ public class AsyncUtil {
         /**
          * 执行失败，或者超时，返回默认值。
          */
-        public T get(long timeout, T defaultValue) {
+        public T get(long timeout, TimeUnit timeUnit, T defaultValue) {
             try {
-                return completableFuture.get(timeout, TimeUnit.MILLISECONDS);
+                return completableFuture.get(timeout, timeUnit);
             } catch (Exception e) {
                 log.error("completableFuture.get error ", e);
             }
             return defaultValue;
         }
 
-        public T get(long timeout) throws Exception {
-            return completableFuture.get(timeout, TimeUnit.MILLISECONDS);
+        public T get(long timeout, TimeUnit timeUnit) throws Exception {
+            return completableFuture.get(timeout, timeUnit);
         }
     }
 
-    public static <U> DefinedCompletableFuture<U> runAsync(Supplier<U> supplier, Executor executor) {
+    public static <U> DefinedCompletableFuture<U> runAsync(Executor executor, Supplier<U> supplier) {
         CompletableFuture<U> completableFuture = CompletableFuture.supplyAsync(supplier, executor);
         return new DefinedCompletableFuture<>(completableFuture);
     }
 
-    public static <T> Future<T> runCallable(Callable<T> callable, ThreadPoolExecutor threadPoolExecutor) {
+    public static <T> Future<T> runCallable(ThreadPoolExecutor threadPoolExecutor, Callable<T> callable) {
         return threadPoolExecutor.submit(callable);
     }
 
-    public static <T> Future<T> runCallable(Callable<T> callable, ThreadPoolTaskExecutor threadPoolTaskExecutor) {
+    public <T> T asyncGet(Executor executor, Supplier<T> supplier, int timeOut, TimeUnit timeUnit, T defaultValue) {
+        DefinedCompletableFuture<T> definedCompletableFuture = AsyncUtil.runAsync(executor, supplier);
+        return definedCompletableFuture.get(timeOut, timeUnit, defaultValue);
+    }
+
+    public static <T> Future<T> runCallable(ThreadPoolTaskExecutor threadPoolTaskExecutor, Callable<T> callable) {
         return threadPoolTaskExecutor.submit(callable);
+    }
+
+    public static <T> Future<T> runAsync(ThreadPoolTaskExecutor threadPoolTaskExecutor, Supplier<T> supplier) {
+        return threadPoolTaskExecutor.submit(supplier::get);
+    }
+
+    public <T> T asyncGet(ThreadPoolTaskExecutor threadPoolTaskExecutor, Supplier<T> supplier, int timeOut, TimeUnit timeUnit) {
+        Future<T> future = AsyncUtil.runAsync(threadPoolTaskExecutor, supplier);
+        try {
+            return future.get(timeOut, timeUnit);
+        } catch (Exception e) {
+            log.error("asyncGet error ", e);
+        }
+        return null;
     }
 }
