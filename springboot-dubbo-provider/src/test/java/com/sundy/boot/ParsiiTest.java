5ff72fc01087e742d994824a935b26ff7ce4e1e9
plus_king <498563079@qq.com>
2024-01-31 16:02:00 +0800

    mapstruct 使用

--- springboot-dubbo-provider/src/test/java/com/sundy/boot/ParsiiTest.java
+++ springboot-dubbo-provider/src/test/java/com/sundy/boot/ParsiiTest.java
@@ -6,6 +6,10 @@ import parsii.eval.Scope;
 import parsii.eval.Variable;
 import parsii.tokenizer.ParseException;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
 /**
  * Created on 2017/11/23
  *
@@ -35,5 +39,16 @@ public class ParsiiTest {
         volume = volume.length() > 6 ? volume.substring(0, volume.length() - 4) + "万" : volume;
 
         System.out.println(volume);
+
+        double d = 123.496782;
+        DecimalFormat decimalFormat = new DecimalFormat("#0.00");
+        System.out.println(decimalFormat.format(d));
+
+        BigDecimal bigDecimal = new BigDecimal("123.496782");
+        System.out.println(bigDecimal.setScale(2, RoundingMode.HALF_UP));
+
+        DecimalFormat df = new DecimalFormat("#0.00");
+        String s = df.format(bigDecimal.doubleValue());
+        System.out.println(s);
     }
 }
