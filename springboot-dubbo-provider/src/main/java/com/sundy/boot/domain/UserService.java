cd903aec0becbb5601f2cf4dbdc8c5a5d892889a
plus_king <498563079@qq.com>
2025-04-30 14:25:15 +0800

    refactor(UserService): 添加Nullness注解并优化缓存构建
    此提交为`UserService`类添加了Nullness注解，并改进了缓存构建的方式，提升了代码的健壯性。

--- springboot-dubbo-provider/src/main/java/com/sundy/boot/domain/UserService.java
+++ springboot-dubbo-provider/src/main/java/com/sundy/boot/domain/UserService.java
@@ -6,6 +6,8 @@ import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.RandomUtils;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Nonnull;
@@ -15,8 +17,11 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 @Component
 public class UserService {
-    private long timeOut = RandomUtils.nextLong(1000, 10000);
-    private long defaultTime = 5 * 60 * 1000;
+
+    private static final long timeOut = RandomUtils.nextLong(1000, 10000);
+
+    private static final long defaultTime = 5 * 60 * 1000;
+
     private LoadingCache<String, UserBean> guavaCache = CacheBuilder.newBuilder()
             .refreshAfterWrite(defaultTime + timeOut, TimeUnit.MILLISECONDS)
             .maximumSize(1000)
@@ -30,7 +35,12 @@ public class UserService {
     private com.github.benmanes.caffeine.cache.LoadingCache<String, UserBean> caffeineCache = Caffeine.newBuilder()
             .refreshAfterWrite(defaultTime + timeOut, TimeUnit.MILLISECONDS)
             .maximumSize(1000)
-            .build(this::getUser);
+            .build(new com.github.benmanes.caffeine.cache.CacheLoader<String, UserBean>() {
+                @Override
+                public @Nullable UserBean load(@NonNull String username) throws Exception {
+                    return getUser(username);
+                }
+            });
 
     public UserBean getUser(String username) {
         // 没有此用户直接返回null
